ef1a9017c41533cdcf65959e6fbc584d4d92dcad
marialejamaap <42081532+marialejamaap@users.noreply.github.com>
2018-08-17 22:57:52 -0500

    Update punto6.cpp

--- punto6.cpp
+++ punto6.cpp
@@ -1,22 +1,19 @@
-// Punto 6.
+// Punto 6.Ordenar alfabeticamente tres string
 
 #include <iostream>
-
 #include <string>
 
 int main()
 
 {
-std::string str1;
-std::string str2;
-std::string str3;
-
-std::cout << "ingresar strings: ";
+std::string str1,str2, str3,x,y,z;
+std::cout << "ingresar strings: \n";
 getline (std::cin, str1);
 getline (std::cin, str2);
 getline (std::cin, str3);
-std::cout << "Escriba palabra 1: ";
-str1>str2?str1>str3?std::cout<<"str1":str2>str1?str2>str3?std::cout<<"str2":str3;
+(str1<str2&&str1<str3)?z=str1:(str1<str2&&str1>str3)||(str1>str2&&str1<str3)?y=str1:x=str1;//posiciones para string 1
+(str2<str1&&str2<str3)?z=str2:(str2<str1&&str2>str3)||(str2>str1&&str2<str3)?y=str2:x=str2;//posiciones para string 2
+(str3<str2&&str3<str1)?z=str3:(str3<str2&&str3>str1)||(str3>str2&&str3<str1)?y=str3:x=str3;// posiciones para string 3
+std::cout<<"\n"<< z <<"\n"<< y <<"\n"<< x;
 return 0;
-
 }
